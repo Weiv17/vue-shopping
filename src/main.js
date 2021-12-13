50bbdcb6a4482ca1bf0a3c10ef143d7bb3026c41
Wei <1402976320@qq.com>
2021-12-13 17:28:05 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -7,8 +7,14 @@ import './plugins/element.js'
 import './assets/css/global.css'
 
 import axios from 'axios'
+// import { config } from 'bluebird'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
